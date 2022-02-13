39c31dbd3b808af3d2672f0ea68aff0409b69750
Dalton Bunker <dbbunker3@gmail.com>
2022-02-13 15:18:49 -0700

    added model schema types

--- models/recipe.js
+++ models/recipe.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose
 
-const recipeSchema = new Schema(
+const commentSchema = new Schema({
+    author: String,
+    comment: String,
+    date: Date
+})
 
-)
+const recipeSchema = new Schema({
+    recipeId: {
+        type: String,
+        required: true
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      enum: [0, 1, 2, 3]
+    },
+    comments: {
+      type: [commentSchema]
+    }
+})
 
 module.exports = mongoose.model("RecipeModel", recipeSchema)
