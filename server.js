b5d1e3fd399235925d40426440a006850c327e56
Dalton Bunker <dbbunker3@gmail.com>
2022-02-14 15:00:10 -0700

    add http operations to recipe and foods

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
+const mongoose = require("mongoose")
 const portNumber = 9000
 
 app.use(express.json())
 app.use(morgan("dev"))
 
-mongoose.connect("mongodb://localhost:27017/whats-for-dinner", () => console.log("Connected to the DB"))
+mongoose.connect("mongodb://localhost:27017/whats-for-dinner-db", () => console.log("Connected to the DB"))
 
-// app.use("/guitars", require("./routes/inventory"))
+// app.use("/foods", require("./routes/foods"))
+app.use("/user", require("./routes/user"))
+// app.use("/recipe", require("./routes/recipe"))
 
 app.use((err, req, res, next) => {
     console.log(err)
